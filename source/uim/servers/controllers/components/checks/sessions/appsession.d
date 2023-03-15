ad6b2436582fd9a8d427d9f6192e1c0188408b72
UIM GIT-Master <ui.manufaktur@gmail.com>
2023-03-15 13:42:45 +0100

    Bugfixing

---
+++ source/uim/servers/controllers/components/checks/sessions/appsession.d
@@ -0,0 +1,38 @@
+/*********************************************************************************************************
+	Copyright: © 2015-2023 Ozan Nurettin Süel (Sicherheitsschmiede)                                        
+	License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file.  
+	Authors: Ozan Nurettin Süel (Sicherheitsschmiede)                                                      
+**********************************************************************************************************/
+module uim.servers.controllers.checks.sessions.appsession;
+
+@safe:
+import uim.servers;
+
+class DSessionExistsCheck : DControllerCheck {
+  mixin(ControllerComponentThis!("SessionExistsCheck"));
+
+  override void initialize(Json configSettings = Json(null)) {
+    super.initialize(configSettings);
+
+    this
+    .redirectUrl("/account");
+  }
+
+  override bool execute(STRINGAA options = null) {    
+    debug writeln(moduleName!DSessionExistsCheck~":DSessionExistsCheck::check");
+
+    if (auto appSession = getSession(options)) {
+      return true;
+    } else { // appsession missing 
+      this.error("appsession_missing");
+      return false; 
+    }
+  }
+}
+mixin(ControllerComponentCalls!("SessionExistsCheck"));
+
+///
+unittest {
+  auto check = new DSessionExistsCheck;
+  assert(check.name == "SessionExistsCheck");
+}
