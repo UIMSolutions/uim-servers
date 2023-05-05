5f6ea33779e9b9727c1c9fa68ab67d9cf5962f43
UIMSolutions <ui.manufaktur@gmail.com>
2023-05-05 15:53:09 +0200

    Updates

--- source/uim/servers/mixins/package..d
+++ source/uim/servers/mixins/package..d
@@ -11,7 +11,7 @@ mixin template ImportUim() {
   public import uim.core;
   public import uim.oop;
   public import uim.html;
-  public import uim.javascript;
+  public import langs.javascript;
   public import uim.bootstrap;
   public import uim.models;
 }
