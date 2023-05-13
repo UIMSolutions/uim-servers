531044ba050bf68ccf0d40e561a40f09e6cd6f11
UIMSolutions <ui.manufaktur@gmail.com>
2023-05-13 18:49:37 +0200

    Updates

--- source/uim/servers/package.d
+++ source/uim/servers/package.d
@@ -26,8 +26,7 @@ public {
 
   import uim.servers.apis;
   import uim.servers.caches;
-  import uim.servers.data.helpers;
-  import uim.servers.data.messages;
+  import uim.servers.classes;
   import uim.servers.data.helpers;
   import uim.servers.data.messages;
   import uim.servers.events;
@@ -51,31 +50,7 @@ bool isValidSession(string sessionToken, string[string] parameters = null) {
 
 DSRVSession[string] srvSessions;
 
-// CRUDI router calls
-template routerEntityCalls(string callName, string controllerName, string serverApp) {
-  const char[] routerEntityCalls = `
-void `~callName~`Index(HTTPServerRequest req, HTTPServerResponse res) {
-  debugMethodCall(moduleName!`~callName~`Index~":`~callName~`Index");
-  `~controllerName~`IndexPageController(`~serverApp~`).request(req, res);
-}
-void `~callName~`Create(HTTPServerRequest req, HTTPServerResponse res) {
-  debugMethodCall(moduleName!`~callName~`Create~":`~callName~`Create");
-  `~controllerName~`CreatePageController(`~serverApp~`).request(req, res);
-}
-void `~callName~`Read(HTTPServerRequest req, HTTPServerResponse res) {
-  debugMethodCall(moduleName!`~callName~`Read~":`~callName~`Read");
-  `~controllerName~`ReadPageController(`~serverApp~`).request(req, res);
-}
-void `~callName~`Update(HTTPServerRequest req, HTTPServerResponse res) {
-  debugMethodCall(moduleName!`~callName~`Update~":`~callName~`Update");
-  `~controllerName~`UpdatePageController(`~serverApp~`).request(req, res);
-}
-void `~callName~`Delete(HTTPServerRequest req, HTTPServerResponse res) {
-  debugMethodCall(moduleName!`~callName~`Delete~":`~callName~`Delete");
-  `~controllerName~`DeletePageController(`~serverApp~`).request(req, res);
-}
-  `;
-}
+
 
 DServer thisServer;
 string[size_t] errorMessages;
